6f3dae49652372936b98a11e55fdd3946816d398
Memy11th <abdelrahmanabdue11@gmail.com>
2024-12-02 12:22:54 +0300

    initializing the project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+function App({children}:{children:React.ReactNode}) {
+
+  return <>
+  {/*components here */}
+  {children}
+  </>
+}
+
+export default App
