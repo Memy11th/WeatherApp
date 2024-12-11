b5e435b2e3d92c67f28cdbd21ac63eee6601fdb9
Memy11th <abdelrahmanabdue11@gmail.com>
2024-12-12 00:00:47 +0300

    J

--- src/components/Essentials/Navbar.tsx
+++ src/components/Essentials/Navbar.tsx
@@ -2,17 +2,19 @@ import ThemeToggle from "../ِAtoms/ThemeToggle";
 
 export function Navbar() {
 
+
+
   return (
-      <>
+    <>
       <header className={`flex justify-between items-center p-4  w-full sticky top-0  backdrop-blur-sm  supports-[backdrop-filter]:bg-background/60 ]  `}  >
         <div>
           <h5 className="font-bold">M11 weather</h5>
         </div>
         <div >
-        <ThemeToggle />
+          <ThemeToggle />
         </div>
-      
+
       </header>
-      </>
+    </>
   );
 }
