60e7007addb4a05cc6574210fe5eff2774cd5199
Memy11th <abdelrahmanabdue11@gmail.com>
2024-12-14 21:28:18 +0300

    :)

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,8 +1,8 @@
 import { LocationError } from "@/components/ِAtoms/LocationError";
 import Reloader from "@/components/ِAtoms/Reloader";
-import { useGeoLocation } from "@/Hooks/useLocation"
+import { useGeoLocation } from "@/Hooks/useLocation";
 import { useWeatherData } from "@/Hooks/useWeatherData";
-import React from 'react'
+import React from 'react';
 
 
 const Home = () => {  
@@ -15,12 +15,12 @@ const Home = () => {
         getLocation();
     },[]);
 
-    if(error && !isLoading){
+    if(error ){
     return <>
         <LocationError error={error} ErrType={`Location Error`} />
     </>
     }  
-    if(!coordinates && !isLoading){
+    if(!coordinates ){
         return(
             <LocationError error='Please enable access to location'  ErrType={`Location Error`} />
 
