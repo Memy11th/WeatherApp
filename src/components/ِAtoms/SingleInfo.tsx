964ccc19feadf3aa19a99cde9b5b22d229c41819
Memy11th <abdelrahmanabdue11@gmail.com>
2024-12-22 21:51:44 +0300

    some logic  added

--- src/components/ِAtoms/SingleInfo.tsx
+++ src/components/ِAtoms/SingleInfo.tsx
@@ -1,8 +1,10 @@
 
-const SingleInfo = ({name,icon,value}:{name:string|null,icon:React.ReactNode|null,value:number|null}) => {
+const SingleInfo = ({name,icon,value}:{name:string,icon:React.ReactNode,value:number}) => {
     return <>
-        <div>
-            
+        <div className="  bg-blue-400/35 dark:bg-slate-900/35 flex justify-between items-center mb-2 rounded-xl p-4 text-muted-foreground">
+            <p>{name}</p>
+            <span>{icon}</span>
+            <span>{value}</span>
         </div>
     </>
 }
--- src/components/ِAtoms/WeatherDetails.tsx
+++ src/components/ِAtoms/WeatherDetails.tsx
@@ -8,62 +8,60 @@ import { format } from "date-fns";
 const WeatherDetails = () => {
     const {coordinates}=useGeoLocation();
     const weatherQuery = useWeatherData<WeatherResponse>(coordinates);
-    const Details = [
+    const Details = weatherQuery.data? [
         {
             name:'Humidity',
             icon:<Droplet/>,
-            value:weatherQuery.data?.main.humidity,
+            value:weatherQuery.data?.main.humidity??0,
             Key:0
         },
         {
             name:'Pressure',
             icon:<Gauge/>,
-            value:weatherQuery.data?.main.pressure,
+            value:weatherQuery.data?.main.pressure??0,
             Key:1
         },
         {
             name:'Temperature',
             icon:<Thermometer/>,
-            value: weatherQuery.data?.main.temp,
+            value: weatherQuery.data?.main.temp??0,
             Key:2
         },
         {
             name:'Feels Like',
             icon:<Thermometer/>,
-            value: weatherQuery.data?.main.feels_like,
+            value: weatherQuery.data?.main.feels_like??0,
             Key:3
         },
         {
             name:'Wind',
             icon:<Compass/>,
-            value:{
-                speed: weatherQuery.data?.wind.speed,
-                direction: weatherQuery.data?.wind.deg
-            },
+            value: weatherQuery.data?.wind.deg??0,
             Key:4
         },
         {
             name:'Sunrise',
             icon:<Sunrise/>,
-            value:format(new Date(weatherQuery.data?.sys.sunrise ?? 0 *1000),'ha'),
+            value:format(new Date(weatherQuery.data?.sys?.sunrise *1000),'hh:mm a') ,
             Key:5
         },
         {
             name:'Sunset',
             icon:<Sunset/>,
-            value:format(new Date(weatherQuery.data?.sys.sunset ?? 0 *1000),'ha'),
+            value:format(new Date(weatherQuery.data?.sys.sunset*1000),'hh:mm a'),
             Key:6
         }
-    ]
+    ] : [];
     return <>
         <div className='col-span-12 md:col-span-5  bg-blue-300/35 dark:bg-slate-800/35 rounded-xl p-4'>
-                {Details.map((item)=>(
+                { Details.map((item)=>(
                     <SingleInfo
-                    key={item.Key}
+                    key={item.Key} 
                     name={item.name} 
                     icon={item.icon} 
                     value={item.value}
                     />
+                    
                     ))}
                 
         </div>
