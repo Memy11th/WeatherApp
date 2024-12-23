c86408bdd59576b83875b6c75a51c03618ca3f5a
Memy11th <abdelrahmanabdue11@gmail.com>
2024-12-23 12:57:05 +0300

    :|

--- src/components/ِAtoms/Forecast.tsx
+++ src/components/ِAtoms/Forecast.tsx
@@ -2,6 +2,7 @@ import { useGeoLocation } from "@/Hooks/useLocation";
 import { useForecast } from "@/Hooks/useWeatherData";
 import { ForecastResponse } from "@/interfaces/ForecastResponse";
 import { format } from "date-fns";
+import { ArrowDown, ArrowUp, Droplet, Wind } from "lucide-react";
 
 interface DailyForecast{
     date: number;
@@ -36,12 +37,48 @@ const Forecast = () => {
             return acc;
         
     },{} as Record<string,DailyForecast> );
-    console.log(dailyForecast)
     const nextFiveDays = Object.values(dailyForecast??{}).slice(1,6);
-    console.log(nextFiveDays);
     return <>
-        <div className="md:col-span-6 col-span-12  bg-blue-300/35 dark:bg-slate-800/35 rounded-xl p-4 space-y-3 ">
+        <div className="md:col-span-6 col-span-12   bg-blue-300/35 dark:bg-slate-800/35 rounded-xl p-4 space-y-3 ">
             <h4 className="text-muted-foreground">5-Days Forecast</h4>
+                <div className="flex flex-col w-full h-full gap-6   ">
+                        {nextFiveDays.map((Day)=>(
+                            <div key={Day.date} className="flex justify-evenly items-center p-3 bg-white dark:bg-slate-900/35 rounded-xl">
+                                <div className="flex flex-col justify-center items-center">
+                                    <span className="text-sm text-muted-foreground">
+                                            {format(new Date(Day.date *1000),'EEE , MMM  d')}
+                                    </span>
+                                    <p className="text-xs text-center">
+                                        {Day.weather.description}
+                                    </p>
+                                </div>
+
+                                <div className="flex justify-start items-center gap-3">
+                                    <div className="flex justify-center items-center">
+                                            <ArrowUp className="h-4 w-4 font-bold text-red-600" />
+                                            <span className="text-sm text-muted-foreground">{Math.round(Day.temp_max)}℃</span>
+                                    </div>
+                                    <div className="flex justify-center items-center">
+                                            <ArrowDown className="h-4 w-4 font-bold text-blue-600" />
+                                            <span className="text-sm text-muted-foreground">{Math.round(Day.temp_min)}℃</span>
+                                    </div>
+                                </div>
+
+                                <div className="flex justify-center items-center gap-3">
+                                        <div className="flex flex-col justify-center items-center">
+                                            <Droplet className="h-4 w-4 text-blue-600" />
+                                            <span className="text-sm text-muted-foreground">{Day.humidity}%</span>
+                                        </div>  
+                                        <div className="flex flex-col justify-center items-center">
+                                            <Wind className="h-4 w-4 text-blue-600" />
+                                            <span className="text-sm text-muted-foreground">{Day.wind}m/s</span>
+                                        </div>
+                                </div>
+                                    
+                            </div>
+                        ))}
+                        
+                </div>
         </div>
     </>
 }
